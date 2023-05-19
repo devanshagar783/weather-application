bf2fe26b861a455458dde4e218c06e025d7d75cd
Devansh Agarwal <devanshagar783@gmail.com>
2023-05-20 00:00:11 +0530

    fixed util function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,17 +1,10 @@
 import axios from "axios";
 import apiKeys from "../apiKeys.js";
 
-export function fetchLocation(location = "") {
-    axios
-        .get(
-            `${apiKeys.base}weather?q=${location}&units=metric&APPID=${apiKeys.key}`
-        )
-        .then((res) => {
-            console.log("devansh res", res);
-            return res?.data.json();
-        })
-        .catch(function (err) {
-            console.log("devansh err", err);
-            return {"error":err}
-        });
-}
+export const fetchLocation = async (location = "") => {
+    const apiCall = await fetch(
+        `${apiKeys.base}weather?q=${location}&units=metric&APPID=${apiKeys.key}`
+    );
+    const data = await apiCall.json();
+    return data;
+};
