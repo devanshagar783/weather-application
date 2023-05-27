8042d3120284918402c69d9367b4065abc6bafea
Devansh Agarwal <devanshagar783@gmail.com>
2023-05-27 23:20:10 +0530

    started working on info component

--- src/components/HighlightsView/index.js
+++ src/components/HighlightsView/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
 
-const HighlightsView = ({data}) => {
-  return (
-    <div className='highlight-item'>
-      {data.title}
-    </div>
-  )
-}
+const HighlightsView = ({ data }) => {
+    return (
+        <div className="highlight-item">
+            {data.title}
+            <div className="highlight-subtext">
+                <img src={data.icon} />
+                <div className="highlight-value">
+                    <p>{data.value}</p>
+                    <p>{data.unit}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default HighlightsView
+export default HighlightsView;
--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,22 +1,61 @@
 import React from "react";
 import "./index.css";
 import HighlightsView from "../HighlightsView";
+import HumidityIcon from "../../assets/humidity.svg";
+import feelsLikeIcon from "../../assets/feelsLike.svg";
+import pressureIcon from "../../assets/pressure.svg";
+import visibilityIcon from "../../assets/visibility.svg";
 
-const Info = ({data}) => {
+const Info = ({ data }) => {
+    const {
+        city,
+        sunrise,
+        sunset,
+        lat,
+        lon,
+        humidity,
+        feelsLike,
+        pressure,
+        visibility,
+        clouds,
+    } = data;
+    console.log("dev data", data);
 
-  const { city, sunrise, sunset, lat, lon, humidity, feelsLike, pressure, visibility, clouds } = data;
-  console.log("dev data", data)
-
-  const ab={
-    "title": "humidity",
-    "icon": "",
-    "value": humidity,
-  }
+    const humidityData = {
+        title: "Humidity",
+        icon: HumidityIcon,
+        value: humidity,
+        unit: "%",
+    };
+    const feelsLikeData = {
+        title: "Feels Like",
+        icon: feelsLikeIcon,
+        value: feelsLike,
+        unit: "°C",
+    };
+    const pressureData = {
+        title: "Pressure",
+        icon: pressureIcon,
+        value: pressure,
+        unit: "hPa",
+    };
+    const visibilityData = {
+        title: "Visibility",
+        icon: visibilityIcon,
+        value: visibility / 1000,
+        unit: "km",
+    };
 
     return (
-        <div className="home-container">
+        <div className="home-container info-container">
             <p className="forecast-text">Todays Highlights</p>
-            <HighlightsView data={ab} />
+            <div></div>
+            <div className="forecast-2row">
+                <HighlightsView data={humidityData} />
+                <HighlightsView data={pressureData} />
+                <HighlightsView data={visibilityData} />
+                <HighlightsView data={feelsLikeData} />
+            </div>
         </div>
     );
 };
